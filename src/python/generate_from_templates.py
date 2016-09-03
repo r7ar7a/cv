711861b3dad3715fdbf4dfd0867354970ca801fc
Rara <aradnai@rapidminer.com>
2016-09-03 22:01:35 +0200

    master template done

--- src/python/generate_from_templates.py
+++ src/python/generate_from_templates.py
@@ -1,16 +1,43 @@
 import yaml
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, StrictUndefined
 import sys
 import os
 
+def select_lang(cfg, n):
+  if isinstance(cfg, dict):
+    get_iterator = cfg.items
+  elif isinstance(cfg, list):
+    get_iterator = lambda: enumerate(cfg)
+  else:
+    return
+  replacements = []
+  for k, v in get_iterator():
+    if isinstance(v, dict) and 'i18n' in v:
+      if 'value' in v:
+        new_val = {v['i18n'][n]: v['value']}
+      else:
+        new_val = v['i18n'][n]
+      replacements.append((k, new_val))
+  for replacement in replacements:
+    cfg[replacement[0]] = replacement[1]
+  for _, v in get_iterator():
+    select_lang(v, n)
+
+      
+
+  
+
 def main():
   from_dir, to_dir, cfg_file = sys.argv[1:]
-  env = Environment(loader = FileSystemLoader(from_dir))
+  env = Environment(loader = FileSystemLoader(from_dir), undefined=StrictUndefined)
 
 
   with open(cfg_file, 'r') as f:
     cfg = yaml.load(f)
-  print(cfg)
+  select_lang(cfg, 0)
+  print(cfg['languages'])
+
+
 
 
   try:
